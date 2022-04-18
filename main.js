4674aa79ee752ab509a4ffa69c658d7d11990737
Lorenzo Galetto <88822812+LoreEatKids@users.noreply.github.com>
2022-04-18 17:50:13 +0200

    buzz changes

--- main.js
+++ main.js
@@ -7,12 +7,22 @@ function info() {
 }
 
 const lightbox = document.createElement("div");
+const next = document.createElement("div")
+next.id = "next"
 lightbox.id = "lightbox";
 document.body.appendChild(lightbox)
+document.body.appendChild(next)
+
+const nextImg = document.createElement("img")
+nextImg.src = "next.svg"
+nextImg.id = "next_img"
+
+next.appendChild(nextImg)
 
 const images = document.querySelector(".img");
 images.addEventListener("click", () => {
     lightbox.classList.add("active")
+    next.classList.add("active")
 
     const lightboxImg = document.createElement("img");
     const arrow = document.createElement("img")
@@ -39,11 +49,30 @@ images.addEventListener("click", () => {
 
     const buzztest = document.querySelector("#buzz")
     buzztest.innerText = "Si chiama buzz :)"
+
+    /* 
+    
+    buzztest.addEventListener("click", () => {
+        console.log("clicked")
+    })
+
+    */
 })
 
 lightbox.addEventListener("click", () => {
     lightbox.classList.remove("active")
+    next.classList.remove("active")
 })
+
+nextImg.addEventListener("click", () => {
+    lightboxImg.src = "doggo2.jpg"
+    lightboxImg.id = "lightboxImg2"
+
+    const buzztest = document.querySelector("#buzz")
+    buzztest.innerText = "È il mio cagnolone"
+})
+
+
 
 
 
